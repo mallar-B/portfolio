ad6b38b041a8bdc6b8211a9d849d20a75ee1fef5
mallar-B <bhattacharyamallar@gmail.com>
2025-03-12 16:04:40 +0530

    add Hero section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,8 +2,26 @@ import Navbar from "@/components/Navbar";
 
 export default function Home() {
   return (
-    <main className="bg-gruvbox-dark-bg0 h-screen">
+    <main className="h-screen bg-gruvbox-dark-bg0">
       <Navbar />
+      <section className="flex h-screen flex-col items-center justify-center bg-gruvbox-dark-bg">
+        <div className="flex flex-col items-start">
+          <span className="p-1 text-2xl text-gruvbox-dark-fg2 font-bold">
+            Hello, I'm
+          </span>
+          <h1 className="text-6xl text-gruvbox-dark-aqua font-bold">
+            Mallar Bhattacharya
+          </h1>
+          <h2 className="self-center pt-2 font-mono text-xl text-gruvbox-dark-orange">
+            {" "}
+            Full Stack Developer
+          </h2>
+          <div className="self-center flex justify-around w-1/2">
+            <button className="border-2 bg-gruvbox-dark-orange border-gruvbox-dark-orange py-2.5 px-4 translate-y-10 rounded-sm">Let's Connect</button>
+            <button className="border-2 text-gruvbox-dark-orange border-gruvbox-dark-orange py-2.5 px-4 translate-y-10 rounded-sm">View My Work</button>
+          </div>
+        </div>
+      </section>
     </main>
   );
 }
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,8 +1,8 @@
 const Navbar = () => {
   return (
-    <header className="flex justify-between px-56 bg-gruvbox-dark-bg1 w-full py-4 pt-6">
-      <span className="font-extrabold text-2xl text-gruvbox-light-bg0">Mallar B.</span>
-      <nav className="text-gruvbox-light-bg0 font-black">
+    <header className="fixed flex w-full justify-between bg-gruvbox-dark-bg1 px-56 py-4 pt-6">
+      <span className="text-2xl font-extrabold text-gruvbox-light-bg0">Mallar B.</span>
+      <nav className="font-black text-gruvbox-light-bg0">
 	<ul className="flex space-x-12">
 	  <li>home</li>
 	  <li>about</li>
