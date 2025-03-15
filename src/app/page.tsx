80aa8cfd90fd7661423f2cbf32698771f0a9d8d7
mallar-B <bhattacharyamallar@gmail.com>
2025-03-15 07:40:53 +0530

    add animations

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,26 +1,69 @@
+"use client";
 import Navbar from "@/components/Navbar";
+import { motion } from "motion/react";
 
 export default function Home() {
   return (
     <main className="h-screen bg-gruvbox-dark-bg0">
       <Navbar />
       <section className="flex h-screen flex-col items-center justify-center bg-gruvbox-dark-bg">
-        <div className="flex flex-col items-start">
-          <span className="p-1 text-2xl text-gruvbox-dark-fg2 font-bold">
+        <motion.div
+          initial={{ filter: "blur(3px)" }}
+          animate={{ filter: "blur(0px)" }}
+          transition={{ delay: 0.5, duration: 0.6 }}
+          className="flex flex-col items-start"
+        >
+          <motion.span
+            initial={{ opacity: 0, x: -30 }}
+            animate={{ opacity: 1, x: 0 }}
+            transition={{ duration: 0.3, delay: 0.2 }}
+            className="p-1 text-2xl text-gruvbox-dark-fg2 font-bold"
+          >
             Hello, I'm
-          </span>
-          <h1 className="text-6xl text-gruvbox-dark-aqua font-bold">
+          </motion.span>
+          <motion.h1
+            initial={{ opacity: 0, x: 30 }}
+            animate={{ opacity: 1, x: 0 }}
+            transition={{ duration: 0.8, delay: 0.4 }}
+            className="text-6xl text-gruvbox-dark-aqua font-bold"
+          >
             Mallar Bhattacharya
-          </h1>
-          <h2 className="self-center pt-2 font-mono text-xl text-gruvbox-dark-orange">
-            {" "}
+          </motion.h1>
+          <motion.h2
+            initial={{ opacity: 0, y: 20 }}
+            animate={{ opacity: 1, y: 0 }}
+            transition={{ duration: 0.8, delay: 0.6 }}
+            className="self-center pt-2 font-mono text-xl text-gruvbox-dark-orange"
+          >
             Full Stack Developer
-          </h2>
-          <div className="self-center flex justify-around w-1/2">
-            <button className="border-2 bg-gruvbox-dark-orange border-gruvbox-dark-orange py-2.5 px-4 translate-y-10 rounded-sm">Let's Connect</button>
-            <button className="border-2 text-gruvbox-dark-orange border-gruvbox-dark-orange py-2.5 px-4 translate-y-10 rounded-sm">View My Work</button>
-          </div>
-        </div>
+          </motion.h2>
+          <motion.div
+            initial={{ opacity: 0, y: 30 }}
+            animate={{ opacity: 1, y: 0 }}
+            transition={{
+              duration: 0.8,
+              delay: 0.8,
+              type: "spring",
+              stiffness: 80,
+            }}
+            className="self-center flex justify-around w-1/2 mt-4"
+          >
+            <motion.button
+              whileHover={{ scale: 1.05 }}
+              whileTap={{ scale: 0.95 }}
+              className="border-2 bg-gruvbox-dark-orange border-gruvbox-dark-orange py-2.5 px-4 rounded-sm"
+            >
+              Let's Connect
+            </motion.button>
+            <motion.button
+              whileHover={{ scale: 1.05 }}
+              whileTap={{ scale: 0.95 }}
+              className="border-2 text-gruvbox-dark-orange border-gruvbox-dark-orange py-2.5 px-4 rounded-sm"
+            >
+              View My Work
+            </motion.button>
+          </motion.div>
+        </motion.div>
       </section>
     </main>
   );
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,15 +1,26 @@
+"use client";
+
+import { motion } from "motion/react";
+
 const Navbar = () => {
   return (
-    <header className="fixed flex w-full justify-between bg-gruvbox-dark-bg1 px-56 py-4 pt-6">
-      <span className="text-2xl font-extrabold text-gruvbox-light-bg0">Mallar B.</span>
+    <motion.header
+      className="fixed top-0 left-0 flex w-full justify-between bg-gruvbox-dark-bg1 px-56 py-4 pt-6"
+      initial={{ y: -100, filter: "blur(3px)" }}
+      animate={{ y: 0, filter: "blur(0px)" }}
+      transition={{ delay: 0.8 , duration: 0.3 }}
+    >
+      <span className="text-2xl font-extrabold text-gruvbox-light-bg0">
+        Mallar B.
+      </span>
       <nav className="font-black text-gruvbox-light-bg0">
-	<ul className="flex space-x-12">
-	  <li>home</li>
-	  <li>about</li>
-	  <li>contact</li>
-	</ul>
+        <ul className="flex space-x-12">
+          <li>home</li>
+          <li>about</li>
+          <li>contact</li>
+        </ul>
       </nav>
-    </header>
+    </motion.header>
   );
 };
 
